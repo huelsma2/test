74877fcb04ae3deed83cb5a4b011672f4b45b753
huelsma2 <huelsma2@ceas-942-mac13.cae.uwm.edu>
2018-01-30 12:44:22 -0600

    setup

---
+++ src/TestSuite.java
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+	FractionTest.class, 
+	FractionTest2.class,
+	FractionTest3.class })
+
+public class TestSuite {
+}
